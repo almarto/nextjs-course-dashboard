8ef43105c7c65ffcc7acf35b9584908326e38d02
almarto <almarto.developer@gmail.com>
2024-01-05 15:27:38 +0100

    chore: enable login

--- auth.config.ts
+++ auth.config.ts
@@ -4,20 +4,19 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
-    // authorized({ auth, request: { nextUrl } }) {
-    authorized() {
-      // const isLoggedIn = !!auth?.user;
-      // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-
+    authorized({ auth, request: { nextUrl } }) {
+      console.log('🚀 TCL ~ file: auth.config.ts:9 ~ authorized ~ auth:', auth);
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
       return true;
-      // if (isOnDashboard) {
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
-      // }
-      // return true;
     },
   },
   providers: [], // Add providers with an empty array for now
